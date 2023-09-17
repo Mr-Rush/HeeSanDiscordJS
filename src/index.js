ddd2e038946ca80d594341d53cc97b901be58c51
PerOHaugstad <134794874+PerOHaugstad@users.noreply.github.com>
2023-09-18 01:20:01 +0200

    mongodb levels

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const { AttachmentBuilder, Client, IntentsBitField, GatewayIntentBits } = require('discord.js');
+const mongoose = require('mongoose');
 const eventHandler = require('./handlers/eventHandler');
 
 const client = new Client({
@@ -8,12 +9,24 @@ const client = new Client({
     IntentsBitField.Flags.GuildMembers,
     IntentsBitField.Flags.GuildMessages,
     IntentsBitField.Flags.MessageContent,
+    IntentsBitField.Flags.GuildPresences,
     GatewayIntentBits.GuildMembers,
   ],
 });
 
+(async () =>{
+  try {
+    mongoose.set(`strictQuery`, false);
+    await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log ("Connected to DB.");
+
+    eventHandler(client);
+    
+    client.login(process.env.TOKEN);
+  } catch (error) {
+    console.log (`Error: ${error}`)
+  }
+})();
 
 
-eventHandler(client);
-
-client.login(process.env.TOKEN);
